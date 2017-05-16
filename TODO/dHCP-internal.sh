d5ae50e6d515a635ee4255f09453c1cb203f2bb5
Antonios Makropoulos <a.makropoulos11@imperial.ac.uk>
2017-05-16 11:26:30 +0100

    update

--- TODO/dHCP-internal.sh
+++
@@ -1,4 +0,0 @@
-
-if [ ! -f dofs/$subj-template-$age-r.dof.gz ];then
-  run mirtk convert-dof dofs/$subj-template-$age-n.dof.gz dofs/$subj-template-$age-r.dof.gz -input-format mirtk -output-format rigid
-fi
