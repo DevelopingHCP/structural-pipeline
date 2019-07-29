f90390dd15f9b333bf45060f16cbdbcd11ac2786
amakropoulos <benuix@gmail.com>
2019-07-29 10:20:33 +0100

    use DrawEM v1.2.1 and MIRTK v1.1

--- scripts/segmentation/pipeline.sh
+++ scripts/segmentation/pipeline.sh
@@ -62,7 +62,7 @@ cd $datadir
 
 if [ ! -f segmentations/${subj}_all_labels.nii.gz ];then
   # run Draw-EM
-  run $DRAWEMDIR/pipelines/neonatal-pipeline.sh $T2 $age -t $threads -c 1 -p 1 -v 1 -atlas $template_name
+  run mirtk neonatal-segmentation $T2 $age -t $threads -c 1 -p 1 -v 1 -atlas $template_name
   echo "----------------------------
 "
 fi
