ac8231e38a4f743020999329b3feb942f421f319
amakropoulos <benuix@gmail.com>
2021-07-13 21:52:23 +0300

    fix max fetal template age

--- parameters/fetal/configuration.sh
+++ parameters/fetal/configuration.sh
@@ -1,7 +1,7 @@
 export TEMPLATE_T2=$DRAWEMDIR/atlases/fetal/T2
 export TEMPLATE_DOFS=$DRAWEMDIR/atlases/fetal/dofs
 export TEMPLATE_MIN_AGE=23
-export TEMPLATE_MAX_AGE=27
+export TEMPLATE_MAX_AGE=37
 export TEMPLATE_CENTER_AGE=30
 curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export TEMPLATE_DEFACE_MASKS=$curr_dir/deface
